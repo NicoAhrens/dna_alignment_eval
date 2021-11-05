421681540a1701ec8d1bde5b05a00ac01f5b6e08
NicoAhrens <nico_ahrens89@web.de>
2021-11-05 18:41:20 +0100

    - changed all the comments to english - put in try and except blocks for better error handling - cleaned up the code a bit (removed comments, tests etc.) - removed test statements from the __main__ program and put them in a test skript

--- main.py
+++ main.py
@@ -1,126 +1,147 @@
-# liste wird fuer die bewertung der eingabe verwendet
-QualityList = ["A", "G", "C", "T", "_", " "]
 
-# creates a dictionary for the evaluation of the mutation in two sequences.
-# the
+### Program for the evaluation of the alignment of two dna sequences ####
+# This program gets two sequences as an input and calculates the alignment of
+# the two corresponding nucleotides at the given position. In the end
+# it prints out the two sequences and in between the line of evaluations
+# of every pair.
 
-# erstellung eines dictionary zur beurteilung der jeweiligen alignments bzw.
-# ihrer konkatinierten strings der sequenzen an der postion [index]. Die
-# jeweiligen Keys sind die letztendliche bewertung. die values sind listen
-# an dessen einzelnen index positionen, die zugehoerigen Alignments bzw.
-# mutationen sind. Es wird ein dictionary verwendet, damit das programm
-# erweiterbar ist
+# Evaluations of nucleotide pairing and there meaning:
+# "|" -> exact match
+# ":" -> a good match
+# "." -> a bad match
+# " " -> nucleotide with gap aligned
+
+# Creates a dictionary for the evaluation of the match of two dna sequences.
+# The keys are the evaluation of the matching pairs of the two dna sequences.
+# the corresponding values lists into which the corresponding pairs for the
+# evaluation are written. So the string "AA" has the corresponding key
+# "|". The dictionary is saved in a global valuable, because then it does not
+# need to be created every time a combination of sequences needs to be
+# evaluated.
 QualityDictionary = \
     {
         "|": ["AA", "GG", "CC", "TT"],
         ":": ["CT", "TC", "AG", "GA"],
         ".": ["CA", "AC", "CG", "GC", "TA", "AT", "TG", "GT"],
-        " ": ["A_", "_A", "G_", "_G", "C_", "_C", "T_", "_T", "__"]
+        " ": ["A_", "_A", "G_", "_G", "C_", "_C", "T_", "_T", "__", "  "]
      }
 
-# zur beurteilung von zwei sequenzen (seq1 und seq2), konkatiniere man hier
-# die einzelnen strings an jeder position. Die Ergebnisse werden anschließend
-# in einer liste gespeichert. die am Ende der Funktion ausgegeben wird, um
-# sie in der alignment_bewertung() verwenden zu koennen.
-# concat pair = seq1[0] + seq2[0]
-# funktion konkatiniert die einzelnen positionen der strings, zu einem
-# stringpaerchen, damit sie im anschluss mit der qalitaetsliste verglichen
-# werden koennen
-# INPUT: str1(), str2()
+
+# This function concatenates every character in the string from one sequence
+# with the corresponding (same position, index) of the other sequence.
+# these pairs are saved in a list. The function returns the whole list to
+# use it in the other functions.
+# INPUT: str seq1, str seq2
 # OUTPUT: list
 def concat_sequnces_in_string(seq1, seq2):
-    string_seq1 = seq1
-    string_seq2 = seq2
+    ###print('This is sequence 1:', seq1)
+    ###print('This is the class of sequence 1:', type(seq1))
+    string_seq1 = seq1.upper()
+    string_seq2 = seq2.upper()
+    concat_list = []
+    index = 0
+    concat_pair = ''
 
-    if len(string_seq1) != len(string_seq2):
-        print("Sequences don't have the same length. Sequence is skipped.")
-    else:
-        concat_list = []
-        index = 0
-        concat_pair = ""
+    # The try and except blocks are for the case, when a string is longer
+    # then the other. Normally an IndexError would occur, when it does
+    # the function just prints out that the two strings do not have the same
+    # length and then continues to concatenate other input sequences. It also
+    # concatenates to the point when the IndexError is raised. The assumption
+    # is that when the user puts in the two sequences, he maybe types in a
+    # wrong character at the end or did not copy the whole sequence.
+    try:
         while index < len(string_seq1) and index < len(string_seq2):
-                for _ in string_seq1:
-                    concat_pair = string_seq1[index] + string_seq2[index]
-                    concat_list.append(concat_pair)
-                    index += 1
-        # print(concat_list) ####TEST####
-        return concat_list
 
-# diese funktion vergleicht die eingespeicherten werte in der liste der
-# sequenzen aus concat_sequences_in_list() mit den werten aus dem
-# QualityDictionary und gibt deren zugehoerigen key in einen string zurueck
-# INPUT: str1(), str2()
+            for nucleotide in string_seq1:
+                concat_pair = string_seq1[index] + string_seq2[index]
+                concat_list.append(concat_pair)
+                index += 1
+
+    except IndexError:
+        print(string_seq1, ' and')
+        print(string_seq2, ' :')
+    return concat_list
+
+# This function takes the actual string pairs from concat_sequences_in_string()
+# and saves it into a variable (concat_list). It then compares every pair of
+# string in the concat_list with the pairs in the dictionary QualityDictionary.
+# It saves the wrong inputs in a list, to print them out later and point out
+# where the positions of the wrong inputs are.
+# When the input is correct it then saves the evaluation of the string pair in
+# a string and returns this string of only evaluations.
+# INPUT: str seq1, str seq2
 # OUTPUT: str
-def alignment_eval(seq1, seq2):
+def alignmentEval(seq1, seq2):
     concat_list = []
 
-    # damit die konkatinierte liste in einer variablen gespeichert wird und
-    # hier an dieser stelle einfacher verwendet werden kann
     concat_list = concat_sequnces_in_string(seq1, seq2)
     index = 0
-    evaluate_string = ""
-    # creates tuple of all items in QualityDictionary, to iterate over the
-    # dictionary.
+    evaluate_string = ''
+
+    # creates tuple of all items in QualityDictionary in reverse order, for
+    # the later iteration
+
     sorted_dict = sorted(QualityDictionary.items(), reverse = True)
     #print(sorted_dict)
     wrong_inputs = []
-    mergedDictionaryValues = \
+    merged_dictionary_values = \
         [
-         "AA", "GG", "CC", "TT", "CT", "TC", "AG", "GA","CA", "AC", "CG", "GC",
-         "TA", "AT", "TG", "GT", "A_", "_A", "G_", "_G", "C_", "_C", "T_",
-         "_T", "__"
+         'AA', 'GG', 'CC', 'TT', 'CT', 'TC', 'AG', 'GA','CA', 'AC', 'CG', 'GC',
+         'TA', 'AT', 'TG', 'GT', 'A_', '_A', 'G_', '_G', 'C_', '_C', 'T_',
+         '_T', '__', '  '
          ]
+    # seq1_seq2_pair is the variable to iterate over in the loop.
+    # it uses each concatenated pair from the concat_list to iterate
+    # over
+
     for seq1_seq2_pair in concat_list:
-        # print('Pair: ', seq1_seq2_pair)
+        ###print('Pair: ', seq1_seq2_pair)
+
         # this if-query creates a list of wrong inputs, when
-        # the seq1_seq2_pair is not in the mergedDictionaryValues and
+        # the seq1_seq2_pair is not in the merged_dictionary_values and
         # puts a # on this position to track the wrong input
 
-        if seq1_seq2_pair not in mergedDictionaryValues:
-            # print('Pair: ', seq1_seq2_pair)
-            # print('QualityValue: ', mergedDictionaryValues)
-            evaluate_string += "#"
-            wrong_inputs.append('Wrong input at #. Character combination {}'\
-                                ' is not allowed'.format(seq1_seq2_pair))
+        if seq1_seq2_pair not in merged_dictionary_values:
+            ###print('Pair: ', seq1_seq2_pair)
+            ####print('QualityValue: ', merged_dictionary_values)
+            evaluate_string += '#'
+            wrong_inputs.append('Character combination {}is not allowed. '\
+                                'Printed out "#" as placeholder.'\
+                                .format(seq1_seq2_pair))
         else:
-            # seq1_seq2_pair is the variable to iterate over in the loop.
-            # it uses each concatenatet pair from the concat_list to
-            # value speichert den jeweiligen concatinierten
-            # string der seq1 und seq2, um diesen mit
-            # der QulitaetsDictionary vergleichen zu koennen
+
+            # This for-loop is for the iteration over the QualityDictionary
+            # which was sorted for easier iteration over it.
+
             for index in range(0, len(sorted_dict), 1):
-            # for-schleife iteriert ueber die gesamte laenge des sortierten
-            # dictionaries
                 #print(sorted_dict[index][1])
 
+                # This for-loop appends the different evaluations for each
+                # concat pair of nucleotides into a string. This string
+                # is returned in the end to use in it in showAlignment
                 for dict_val in sorted_dict[index][1]:
                     if seq1_seq2_pair == dict_val:
                         evaluate_string += sorted_dict[index][0]
+
     for index in wrong_inputs:
         print([index][0])
+
     return evaluate_string
-# diese funktion druckt die einzelnen sequenzen wieder aus und fuegt die
-# bewertungen zwischen jedem vorher bewerteten stringpaerchen aus
-# INPUT: str1(), str2()
+# This function prints out each dna sequence and in between it prints out
+# the match evaluation. If the sequences do not have the same length it prints
+# out this information for the user.
+# INPUT: str seq1, str seq2
 # OUTPUT: None
 def showAlignment(seq1, seq2):
-    evaluation = alignment_eval(seq1, seq2)
-    # evaluation = alignment_bewertung(seq1,seq2)
+    evaluation = alignmentEval(seq1, seq2)
+    if len(seq1) == len(seq2):
+        print('Alignment Evaluation:')
+        print(len(seq1)* '~')
+        print(seq1)
+        print(evaluation)
+        print(seq2)
+        print(len(seq1) * '~')
+    else:
+        print('Did not align sequences, because they are not the same length')
 
-    print(seq1)
-    print(evaluation)
-    print(seq2)
-
-# die einzelnen sequenzen muessen hier wieder getrennt werden ODER
-# sie muessen von anderen funktionen uebergeben werden
-
-
-# Liste zur Kontrolle ob eine Aminosäuresequenz eingegeben wurde
-
-if __name__ == "__main__":
-    seq1 = "TTCGZU"
-    seq2 = "TCSGSS"
-    showAlignment(seq1, seq2)
-    seq1 = "ATT"
-    seq2 = "ATG"
-    showAlignment(seq1, seq2)
+if __name__ == '__main__':
