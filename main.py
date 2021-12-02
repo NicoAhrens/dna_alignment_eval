b0adc6dd39e3e8493b5cbc0831a2a746454ce47f
NicoAhrens <nico_ahrens89@web.de>
2021-12-02 08:21:42 +0100

    - cleaned up the code

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-
-### Program for the evaluation of the alignment of two dna sequences ####
+# ~~~ Program for the evaluation of the alignment of two dna sequences ~~~ #
 # This program gets two sequences as an input and calculates the alignment of
 # the two corresponding nucleotides at the given position. In the end
-# it prints out the two sequences and in between the line of evaluations
-# of every pair.
+# it prints out the two sequences and in between of those two sequences
+# the line of evaluations of every pair.
 
 # Evaluations of nucleotide pairing and there meaning:
 # "|" -> exact match
@@ -24,7 +23,7 @@ QualityDictionary = \
         ":": ["CT", "TC", "AG", "GA"],
         ".": ["CA", "AC", "CG", "GC", "TA", "AT", "TG", "GT"],
         " ": ["A_", "_A", "G_", "_G", "C_", "_C", "T_", "_T", "__", "  "]
-     }
+    }
 
 
 # This function concatenates every character in the string from one sequence
@@ -34,8 +33,8 @@ QualityDictionary = \
 # INPUT: str seq1, str seq2
 # OUTPUT: list
 def concat_sequnces_in_string(seq1, seq2):
-    ###print('This is sequence 1:', seq1)
-    ###print('This is the class of sequence 1:', type(seq1))
+    # TEST: print('This is sequence 1:', seq1)
+    # TEST: print('This is the class of sequence 1:', type(seq1))
     string_seq1 = seq1.upper()
     string_seq2 = seq2.upper()
     concat_list = []
@@ -62,6 +61,7 @@ def concat_sequnces_in_string(seq1, seq2):
         print(string_seq2, ' :')
     return concat_list
 
+
 # This function takes the actual string pairs from concat_sequences_in_string()
 # and saves it into a variable (concat_list). It then compares every pair of
 # string in the concat_list with the pairs in the dictionary QualityDictionary.
@@ -71,7 +71,7 @@ def concat_sequnces_in_string(seq1, seq2):
 # a string and returns this string of only evaluations.
 # INPUT: str seq1, str seq2
 # OUTPUT: str
-def alignmentEval(seq1, seq2):
+def alignment_eval(seq1, seq2):
     concat_list = []
 
     concat_list = concat_sequnces_in_string(seq1, seq2)
@@ -82,44 +82,46 @@ def alignmentEval(seq1, seq2):
     # the later iteration
 
     sorted_dict = sorted(QualityDictionary.items(), reverse=True)
-    #print(sorted_dict)
+    # TEST: print(sorted_dict)
     wrong_inputs = []
     merged_dictionary_values = \
         [
-         'AA', 'GG', 'CC', 'TT', 'CT', 'TC', 'AG', 'GA','CA', 'AC', 'CG', 'GC',
-         'TA', 'AT', 'TG', 'GT', 'A_', '_A', 'G_', '_G', 'C_', '_C', 'T_',
-         '_T', '__', '  '
-         ]
+            'AA', 'GG', 'CC', 'TT', 'CT', 'TC', 'AG', 'GA', 'CA', 'AC', 'CG',
+            'GC',
+            'TA', 'AT', 'TG', 'GT', 'A_', '_A', 'G_', '_G', 'C_', '_C', 'T_',
+            '_T', '__', '  '
+        ]
     # seq1_seq2_pair is the variable to iterate over in the loop.
-    # it uses each concatenated pair from the concat_list to iterate
-    # over
+    # It uses each concatenated pair from the concat_list to iterate over
 
     for seq1_seq2_pair in concat_list:
-        ###print('Pair: ', seq1_seq2_pair)
+        # TEST: print('Pair: ', seq1_seq2_pair)
 
         # this if-query creates a list of wrong inputs, when
         # the seq1_seq2_pair is not in the merged_dictionary_values and
         # puts a # on this position to track the wrong input
 
         if seq1_seq2_pair not in merged_dictionary_values:
-            ###print('Pair: ', seq1_seq2_pair)
-            ####print('QualityValue: ', merged_dictionary_values)
+            # TEST: print('Pair: ', seq1_seq2_pair)
+            # TEST: print('QualityValue: ', merged_dictionary_values)
             evaluate_string += '#'
-            wrong_inputs.append('Character combination {} in the following sequence alignment'\
-                                ' is not allowed. '\
-                                'Printed out "#" as placeholder.'\
-                                .format(seq1_seq2_pair))
+            wrong_inputs.append(
+                'Character combination {} in the following sequence '
+                'alignment' 
+                ' is not allowed. ' 
+                'Printed out "#" as placeholder.'
+                .format(seq1_seq2_pair))
         else:
 
             # This for-loop is for the iteration over the QualityDictionary
             # which was sorted for easier iteration over it.
 
             for index in range(0, len(sorted_dict), 1):
-                #print(sorted_dict[index][1])
+                # TEST: print(sorted_dict[index][1])
 
                 # This for-loop appends the different evaluations for each
                 # concat pair of nucleotides into a string. This string
-                # is returned in the end to use in it in showAlignment
+                # is returned in the end to use in it in show_alignment
                 for dict_val in sorted_dict[index][1]:
                     if seq1_seq2_pair == dict_val:
                         evaluate_string += sorted_dict[index][0]
@@ -129,13 +131,14 @@ def alignmentEval(seq1, seq2):
 
     return evaluate_string
 
+
 # This function prints out each dna sequence and in between it prints out
 # the match evaluation. If the sequences do not have the same length it prints
 # out this information for the user.
 # INPUT: str seq1, str seq2
 # OUTPUT: None
-def showAlignment(seq1, seq2):
-    evaluation = alignmentEval(seq1, seq2)
+def show_alignment(seq1, seq2):
+    evaluation = alignment_eval(seq1, seq2)
     if len(seq1) == len(seq2):
         print('\nAlignment Evaluation:')
         print(len(seq1) * '~')
@@ -144,4 +147,5 @@ def showAlignment(seq1, seq2):
         print(seq2)
         print(len(seq1) * '~')
     else:
-        print('Did not align sequences, because they are not the same length.\n')
+        print(
+            'Did not align sequences, because they are not the same length.\n')
--- test.py
+++ test.py
@@ -1,41 +1,41 @@
 
-### Different tests for showAlignment() ###
+# ~~~ Different tests for show_alignment() ~~~ #
 
 import main
 
 seq1 = 'AGTCAAT'
 seq2 = 'AGTCAAT'
-main.showAlignment(seq1, seq2)
+main.show_alignment(seq1, seq2)
 seq1 = 'AAAAAA'
 seq2 = 'AAAAAA'
-main.showAlignment(seq1, seq2)
+main.show_alignment(seq1, seq2)
 seq1 = '_AAAAA'
 seq2 = 'AAAAAA'
-main.showAlignment(seq1, seq2)
+main.show_alignment(seq1, seq2)
 seq1 = 'AGTCTA'
 seq2 = 'GTCGAC'
-main.showAlignment(seq1, seq2)
+main.show_alignment(seq1, seq2)
 seq1 = 'TTCGZU'
 seq2 = 'TCSGTS'
-main.showAlignment(seq1, seq2)
+main.show_alignment(seq1, seq2)
 seq1 = 'ATTa'
 seq2 = 'ATGA'
-main.showAlignment(seq1, seq2)
+main.show_alignment(seq1, seq2)
 seq1 = 'AT T'
 seq2 = 'AT G'
-main.showAlignment(seq1, seq2)
+main.show_alignment(seq1, seq2)
 seq1 = 'AT_T__AGTC'
 seq2 = 'AT_G_AATGC'
-main.showAlignment(seq1, seq2)
+main.show_alignment(seq1, seq2)
 seq1 = '___'
 seq2 = '___'
-main.showAlignment(seq1, seq2)
+main.show_alignment(seq1, seq2)
 seq1 = 'ATTTTTTTTTTTTGGGGGGGGtgtgat'
 seq2 = 'ATGGGGGGGGGTTTTTTTTTTg'
-main.showAlignment(seq1, seq2)
+main.show_alignment(seq1, seq2)
 seq1 = '123'
 seq2 = '456'
-main.showAlignment(seq1, seq2)
+main.show_alignment(seq1, seq2)
 seq1 = '#+??ß'
 seq2 = '#+??ß'
-main.showAlignment(seq1, seq2)
+main.show_alignment(seq1, seq2)
